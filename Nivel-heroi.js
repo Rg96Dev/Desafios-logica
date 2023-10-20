020b73989fd903862cb655c21455deedcd95c56c
Rg96Dev <rafalgui96@gmail.com>
2023-10-20 20:56:50 -0300

    meu primeiro commit

---
+++ Nivel-heroi.js
@@ -0,0 +1,27 @@
+let nome = "Cratus"
+let xp = 9.550
+
+if (xp <= 1.000) {
+    console.log("o heroi " + nome + " está no nivél de Ferro");
+}else if (xp >= 1.001 && xp <= 2.000) {
+    console.log("o heroi " + nome + " está no nivé de Bronze");
+} else if (xp >= 2.001 && xp <= 5.000) {
+    console.log("o heroi " + nome + " está no nivé de Prata");
+} else if (xp >= 5.001 && xp <= 6.000) {
+    console.log("o heroi " + nome + " está no nivé de Ouro");
+} else if (xp >= 6.001 && xp <= 7.000) {
+    console.log("o heroi " + nome + " está no nivé de Platina");
+} else if (xp >= 7.001 && xp <= 8.000) {
+    console.log("o heroi " + nome + " está no nivé de Ascendente");
+} else if (xp >= 8.001 && xp <= 9.000) {
+    console.log("o heroi " + nome + " está no nivé de Imortal");
+} else if (xp >= 9.001 && xp <= 10.000) {
+    console.log("o heroi " + nome + " está no nivé de Radiante");
+}
+
+
+
+
+
+
+
